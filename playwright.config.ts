2f5a2e7339ade50a8b29e2adccc2d5b4383843fd
junkei_okinawa <ceazy.x2.okinawan@gmail.com>
2025-12-08 15:02:29 +0900

    chore(config): add explicit timeouts for Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,8 +7,10 @@ export default defineConfig({
     retries: process.env.CI ? 2 : 0,
     workers: process.env.CI ? 1 : undefined,
     reporter: 'html',
+    timeout: 60000,
     use: {
         baseURL: 'http://localhost:3000',
+        timeout: 30000,
         trace: 'on-first-retry',
     },
     projects: [
